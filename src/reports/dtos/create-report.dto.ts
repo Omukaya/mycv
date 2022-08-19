c5b87b3c48a0e2386b09c5c79538e460fcb2bdb6
Omukaya <charlesomukaya3@gmail.com>
2022-08-19 17:14:40 +0300

    Receiving Report Creation Requests

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,17 +1,37 @@
-import { IsString, IsNumber } from 'class-validator';
+import {
+  IsString,
+  IsNumber,
+  Min,
+  Max,
+  IsLongitude,
+  IsLatitude,
+} from 'class-validator';
 
-export class CreateReortDto {
+export class CreateReportDto {
+  @IsString()
   make: string;
 
+  @IsString()
   model: string;
 
+  @IsNumber()
+  @Min(2005)
+  @Max(2022)
   year: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   mileage: number;
 
+  @IsLongitude()
   lng: number;
 
+  @IsLatitude()
   lat: number;
 
+  @IsNumber()
+  @Min(0)
+  @Max(1000000)
   price: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('reports')
+@UseGuards(AuthGuard)
 export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
   @Post()
-  createReport(@Body() body: CreateReortDto) {}
+  createReport(@Body() body: CreateReportDto) {
+    return this.reportsService.create(body);
+  }
 }
