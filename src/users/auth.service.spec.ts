831108b77f96631bb1524458e55a4d6a2f897b2e
Omukaya <charlesomukaya3@gmail.com>
2022-08-19 16:40:38 +0300

    Adding properties to reports

---
+++ src/users/auth.service.spec.ts
@@ -0,0 +1,5 @@
+import { Test } from '@nestjs/testing';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+
+it('can create an instance of auth service', () => {});
